1692e9b8fd65d2b8621ab98a0fd6768400cf972e
Tuananh2810 <trantuananh.28102002@gmail.com>
2024-07-30 16:04:21 +0700

    setup base project

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+// router
+import { RouterProvider } from "react-router-dom";
+import { Route } from "react-router-dom";
+
 import "./config/style/tailwind.css";
 import "./config/style/modern-normalize.css";
 import "./config/style/font-family.css";
 import "./config/style/main.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<App />
+		<RouterProvider Route={Route} />
 	</React.StrictMode>,
 );
