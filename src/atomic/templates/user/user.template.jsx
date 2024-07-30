50574d27a4bcdd3299433a01099a477e8cc287ed
Tuananh2810 <trantuananh.28102002@gmail.com>
2024-07-30 21:09:17 +0700

    header

--- src/atomic/templates/user/user.template.jsx
+++ src/atomic/templates/user/user.template.jsx
@@ -1,19 +1,24 @@
 import { Link } from "react-router-dom";
-import { Select } from "../../atoms/select";
+import { SelectTranslate } from "./components/translate/translate";
+
 export function UserTemplate({ children }) {
 	return (
 		<>
-			<header className="h-24 px-16 bg-black text-white sticky">
-				<Link to={"/"}>
-					<img className="h-[3.2rem]" src="/logo.webp" alt="logo" />
-				</Link>
+			<header className="h-24 px-16 bg-black text-white sticky flex items-center justify-between">
+				<div className="flex items-center gap-12">
+					<Link to={"/"}>
+						<img className="h-[3.2rem]" src="/logo.webp" alt="logo" />
+					</Link>
 
-				<nav>
-					<Link to={"/"}>Home</Link>
-					<Link to="/">Phim Đang Chiếu</Link>
-					<Link>Phim Chuẩn Bị Chiếu</Link>
-				</nav>
-				<Select />
+					<nav className="flex gap-4">
+						<Link to="/">Trang Chủ</Link>
+						<Link to="/showing-movie">Phim Đang Chiếu</Link>
+						<Link to="/coming-movie">Phim Chuẩn Bị Chiếu</Link>
+					</nav>
+				</div>
+				<div>
+					<SelectTranslate />
+				</div>
 			</header>
 			{children}
 		</>
